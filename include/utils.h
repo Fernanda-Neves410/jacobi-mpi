207bea432e782dda3a2684d15048e39315f1f701
Fernanda Neves <fernanda_neves@ufms.br>
2026-06-15 17:02:58 -0400

    Uso correto da matriz
    atualizei o código para que a matriz seja transformada em vetor para facilitar os cálculos do mpi

--- include/utils.h
+++ include/utils.h
@@ -7,6 +7,10 @@ using namespace std;
 // Gera uma matriz diagonal dominante
 void gerar_matriz(vector<vector<double>>& A, vector<double>& b, int n);
 
+void converter_matriz_para_vetor(vector<vector<double>>& A,
+                                double* A_linear,
+                                int n);
+
 // Calcula diferença entre resultado sequencial e MPI
 double calcular_diferenca(vector<double>& resultado_seq,
                           vector<double>& resultado_mpi,
--- src/utils.cpp
+++ src/utils.cpp
@@ -1,9 +1,16 @@
 #include "utils.h"
+
 #include <cmath>
 
-void gerar_matriz(vector<vector<double>>& A,
-                  vector<double>& b,
-                  int n) {
+using namespace std;
+
+
+// Gera uma matriz diagonal dominante
+void gerar_matriz(
+    vector<vector<double>>& A,
+    vector<double>& b,
+    int n
+) {
 
     for (int i = 0; i < n; i++) {
 
@@ -12,7 +19,9 @@ void gerar_matriz(vector<vector<double>>& A,
         for (int j = 0; j < n; j++) {
 
             if (i != j) {
+
                 A[i][j] = 1.0;
+
                 soma_linha += A[i][j];
             }
         }
@@ -23,6 +32,26 @@ void gerar_matriz(vector<vector<double>>& A,
     }
 }
 
+
+// Converte matriz 2D para vetor linear
+void converter_matriz_para_vetor(
+    vector<vector<double>>& A,
+    double* A_linear,
+    int n
+) {
+
+    for (int linha = 0; linha < n; linha++) {
+
+        for (int coluna = 0; coluna < n; coluna++) {
+
+            A_linear[linha * n + coluna] =
+                A[linha][coluna];
+        }
+    }
+}
+
+
+// Calcula diferença entre resultado sequencial e MPI
 double calcular_diferenca(
     vector<double>& resultado_seq,
     vector<double>& resultado_mpi,
@@ -34,8 +63,8 @@ double calcular_diferenca(
     for (int i = 0; i < n; i++) {
 
         diferenca += abs(
-            resultado_seq[i] -
-            resultado_mpi[i]
+            resultado_seq[i]
+            - resultado_mpi[i]
         );
     }
 
