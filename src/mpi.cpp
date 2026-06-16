0bbf2dcecfaf87041dc7e0f7ede38af64ca45f82
Fernanda Neves <fernanda_neves@ufms.br>
2026-06-15 20:43:27 -0400

    Update mpi.cpp

--- src/mpi.cpp
+++ src/mpi.cpp
@@ -1,4 +1,5 @@
 #include "jacobi.h"
+#include "utils.h"
 
 #include <mpi.h>
 #include <vector>
@@ -9,6 +10,7 @@ using namespace std;
 vector<double> executar_jacobi_mpi(
     vector<vector<double>>& A,
     vector<double>& b,
+    int n,
     int max_iter,
     double epsilon
 ) {
@@ -19,69 +21,104 @@ vector<double> executar_jacobi_mpi(
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_processos);
 
-    int n = A.size();
 
-    vector<double> x_old(n, 0.0);
-    vector<double> x_new(n, 0.0);
+    int n = n;
 
 
+    // Converte a matriz para formato linear
+    double* A_linear = nullptr;
 
-    // Distribuição simples das linhas
-    int linhas_por_processo = n / num_processos;
+    if(rank == 0) {
 
-    int inicio = rank * linhas_por_processo;
+        A_linear = new double[n * n];
 
-    int fim;
+        converter_matriz_para_vetor(
+            A,
+            A_linear,
+            n
+        );
+    }
 
-    if(rank == num_processos - 1)
-        fim = n;
-    else
-        fim = inicio + linhas_por_processo;
+
+
+    // Quantidade de linhas deste processo
+    int linhas_locais = n / num_processos;
+
+    if(rank == num_processos - 1) {
+
+        linhas_locais += n % num_processos;
+    }
+
+
+
+    // Dados locais
+    vector<double> A_local(
+        linhas_locais * n
+    );
+
+    vector<double> b_local(
+        linhas_locais
+    );
+
+
+
+    // Vetores do método de Jacobi
+    vector<double> x_old(
+        n,
+        0.0
+    );
+
+    vector<double> x_new(
+        n,
+        0.0
+    );
+
+
+
+    // TODO:
+    // MPI_Scatterv para distribuir A_local
+
+    // TODO:
+    // MPI_Scatterv para distribuir b_local
 
 
 
     for(int k = 0; k < max_iter; k++) {
 
         double erro_local = 0.0;
+
         double norma_local = 0.0;
 
 
-        // Cada processo calcula suas linhas
-        for(int i = inicio; i < fim; i++) {
 
-            double soma = 0.0;
-
-            for(int j = 0; j < n; j++) {
-
-                if(j != i)
-                    soma += A[i][j] * x_old[j];
-            }
-
-            x_new[i] =
-                (b[i] - soma) / A[i][i];
-
-            erro_local +=
-                pow(x_new[i] - x_old[i], 2);
-
-            norma_local +=
-                pow(x_new[i], 2);
-        }
+        // TODO:
+        // calcular apenas as linhas locais
 
 
 
-        // trocar partes de x_new entre processos
+        // TODO:
+        // reconstruir x_new completo
 
 
 
+        // TODO:
         // calcular erro global
 
 
 
+        // TODO:
         // verificar convergência
 
 
 
         x_old = x_new;
+    }
+
+
+
+    if(rank == 0 && A_linear != nullptr) {
+
+        delete[] A_linear;
     }
 
     return x_new;
